f52221071cf2da10400dbd4f28a1edc04b6ad1ea
unknown <mohammadimohammadamin864@gmail.com>
2023-03-08 21:52:31 +0330

    ''

---
+++ scmk/web/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Expense(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()    
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
+    def __unicode__(self):
+        return "{}-{}-{}".format(self.date, self.user, self.amount)
+
+
+class Income(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
+
+    def __unicode__(self):
+        return "{}-{}-{}".format(self.date, self.user, self.amount)
