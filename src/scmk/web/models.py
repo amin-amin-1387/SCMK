86dd90f1c498eb3f38949938b062786a15e61da4
unknown <mohammadimohammadamin864@gmail.com>
2023-03-13 17:11:23 +0330

    ''

--- src/scmk/web/models.py
+++ src/scmk/web/models.py
@@ -1,7 +1,14 @@
+from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Token(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    token = models.CharField(max_length=80)
+    def __unicode__(self):
+        return f"{self.user}"
+
 class Expense(models.Model):
     text = models.CharField(max_length=255)
     date = models.DateTimeField()
--- src/scmk/web/views.py
+++ src/scmk/web/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 
-def index(request):
+def submit_expense(request):
     return HttpResponse("HELLO_WORLD")
